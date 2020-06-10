0008962482aad83f0adffac7cd87f034e2994215
九日木彡 <nofacer@outlook.com>
2020-06-10 19:45:37 +0800

    Extract a clone_repo method

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def get_token(file_path='token.txt'):
     return token
 
 
-if __name__ == '__main__':
-    git_url = 'https://{}:x-oauth-basic@github.com/nofacer/blog.articles'.format(get_token())
-    dest_dir = './download'
-
+def clone_repo(git_url: str, dest_dir: str):
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
     os.mkdir(dest_dir)
-
     Repo.clone_from(git_url, dest_dir)
+
+
+if __name__ == '__main__':
+    clone_repo('https://{}:x-oauth-basic@github.com/nofacer/blog.articles'.format(get_token()), './download')
