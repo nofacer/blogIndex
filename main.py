2020e12fc00878b97bf47b837a4cecd98e9363e7
九日木彡 <nofacer@outlook.com>
2020-06-10 19:52:11 +0800

    Add some type annotations

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import os
 import shutil
 
 
-def get_token(file_path='token.txt'):
+def get_token(file_path='token.txt') -> str:
     with open(file_path, "r") as f:
         token = f.read()
     return token
 
 
-def recreate_folder(dest_dir):
-    if os.path.exists(dest_dir):
-        shutil.rmtree(dest_dir)
-    os.mkdir(dest_dir)
+def recreate_folder(target_dir: str) -> None:
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)
+    os.mkdir(target_dir)
 
 
-def clone_repo(git_url: str, dest_dir: str):
-    recreate_folder(dest_dir)
-    Repo.clone_from(git_url, dest_dir)
+def clone_repo(git_url: str, target_dir: str) -> None:
+    recreate_folder(target_dir)
+    Repo.clone_from(git_url, target_dir)
 
 
 if __name__ == '__main__':
