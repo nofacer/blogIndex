a93902bd84466d55f4000761c8b88c3a609fe1ff
九日木彡 <nofacer@outlook.com>
2020-06-16 15:26:25 +0800

    Use flask to provide service

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from SqliteIndexer import *
 from Downloader import Downloader
 from Parser import parse
+from flask import Flask
 
-if __name__ == '__main__':
+app = Flask(__name__)
+
+
+@app.route('/', methods=['get'])
+def hello():
+    return {'message': 'hello world'}, 200
+
+
+@app.route('/index', methods=['POST'])
+def index_articles():
+    index()
+    return {'message': 'index articles successfully'}, 200
+
+
+def index():
     DEST_DIR = 'download'
     GIT_USER = 'nofacer'
     REPO_NAME = 'blog.articles'
@@ -18,3 +33,7 @@ if __name__ == '__main__':
         article_metadata = article_info_tuple[0]
         article_string = article_info_tuple[1]
         insert_data(article_metadata, article_string)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
